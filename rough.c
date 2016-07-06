3b17fac43791a9110cb5d31db9bac0bad650f5e9
HeyoCool <heyocool@gmail.com>
2016-07-06 14:57:13 +0530

    The first c program

---
+++ rough.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	for(i=0;i<10;i++)
+	{
+		for(j=0;j<10;j++)
+			if(a>=b)
+				print("*");
+			else
+				print(" ");
+		print("\n");
+	}
+	return 0;
+}
